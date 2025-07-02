59d1e4978903425388530449c9ac057b8b93531b
asemov12 <a.m.semov@tudelft.nl>
2025-07-02 13:09:34 +0300

    Implements functionalities for the services

--- LunchMenu.Repository/Helpers/OrderDish/OrderDishFilter.cs
+++ LunchMenu.Repository/Helpers/OrderDish/OrderDishFilter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,7 @@ namespace LunchMenu.Repository.Helpers.OrderDish
 {
     public class OrderDishFilter
     {
+        public SqlInt32? OrderId { get; set; }
+        public SqlInt32? DishId { get; set; }
     }
 }
